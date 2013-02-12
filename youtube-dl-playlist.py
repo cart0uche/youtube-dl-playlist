d378d5810adb9ca9a9bfa2f3297dde5a480e25f8
Hafid Saou <hafid.saou@gmail.com>
2013-02-13 00:27:09 +0100

    Ajout d'une interface graphique  pour la séléction de la playlist

--- youtube-dl-playlist.py
+++ youtube-dl-playlist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 # youtube-dl-playlist
-
 
 import os
 import sys
@@ -8,6 +8,9 @@ import ConfigParser
 import argparse
 import glob
 import shutil
+import Tkinter
+import ttk
+
 
 dirVideoName = ".video"
 
@@ -21,7 +24,7 @@ def getPlaylistCode(config, playListName):
    return None
 
 
-def setFolder(playlistName):
+def createFolder(playlistName):
    if os.path.isdir(playlistName) == False:
       os.mkdir(playlistName)
       os.mkdir(os.path.join(playlistName, dirVideoName))
@@ -35,7 +38,7 @@ def updatePlaylist(config, playlistName):
       return
 
    # Create folder if needed
-   setFolder(playlistName)
+   createFolder(playlistName)
 
    # Call to youtube-dl
    os.chdir(os.path.join(playlistName, dirVideoName))
@@ -52,6 +55,7 @@ def updatePlaylist(config, playlistName):
    for videoFile in videoFiles:
       open(videoFile,'w')
    os.chdir(os.pardir)
+   os.chdir(os.pardir)
 
 
 def updatePlaylists(config, playlistsName):
@@ -64,6 +68,40 @@ def updateAllPlaylist(config):
       for (key, value) in config.items(section):
          updatePlaylist(config, key)
 
+class DisplayInterface(Tkinter.Tk):
+
+   def __init__(self,parent,config):
+      Tkinter.Tk.__init__(self,parent)
+      self.parent = parent
+      self.config = config
+      self.initialize()
+
+   def initialize(self):
+      self.grid()
+
+      self.message = Tkinter.Message(self,text="Select a playlist :",width=200)
+      self.message.grid(padx=10,pady=10,column=0,row=0,sticky='W')
+
+      var = Tkinter.StringVar()
+      playListvar = [p for p in self.config.options('Music')]
+      var.set(playListvar[0])
+      self.play = ttk.OptionMenu(self.parent, var, *playListvar)
+      self.play.grid(padx=10,pady=10,column=0,row=1,sticky='WENS')
+
+      buttonDownload = Tkinter.Button(self,text=u"Download",command=lambda x=var:self.OnButtonClick(var.get()))
+      buttonDownload.grid(column=0,row=2)
+
+      self.title("youtube-dl-playlist")
+      self.protocol("WM_DELETE_WINDOW", self.closeWindows)
+      self.mainloop()
+
+   def OnButtonClick(self,playList):
+      print "# %s selected" % playList
+      updatePlaylist(self.config, playList)
+
+   def closeWindows(self):
+      self.destroy()
+
 
 def main():
    iniFile = "playlist.ini"
@@ -72,11 +110,12 @@ def main():
    parser = argparse.ArgumentParser(description='Download music from your Youtube playlist.')
    parser.add_argument('-a', '--all', action='store_true', help="Update all your playlist")
    parser.add_argument('-p', '--playlist', nargs='*', default=None, action='store', help="Update specific playlist")
+   parser.add_argument('-g', '--gui', action='store_true', help="Display graphic interface")
    args = parser.parse_args()
 
    # check args
    if not (args.all or args.playlist):
-      parser.error('No action required, add --all or --playlist')
+      args.gui=1
 
    # Open playlist.ini
    config = ConfigParser.SafeConfigParser()
@@ -84,6 +123,10 @@ def main():
       config.read(iniFile)
    else:
       print "%s does not exist" % iniFile
+      return
+
+   if args.gui :
+      app = DisplayInterface(None, config=config)
       return
 
    # Update playlist
