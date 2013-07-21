03decb401278f661aada164309a227da71454076
Hafid Saou <hafid.saou@gmail.com>
2013-07-21 18:06:50 +0200

    Ajout des paramètres output_path, audio_quality et audio_format

--- youtube-dl-playlist.py
+++ youtube-dl-playlist.py
@@ -14,40 +14,49 @@ import PyZenity
 dirVideoName = ".video"
 
 
-def getPlaylistCode(config, playListName):
+def getPlaylistCode(config, playlist_name):
 	for section in config.sections():
 		for (key, value) in config.items(section):
-			if playListName == key:
+			if playlist_name == key:
 				return value
-	print "%s was not found. Add this playlist in your playlist.ini file." % playListName
+	print "%s was not found. Add this playlist in your playlist.ini file." % playlist_name
 	return None
 
 
-def createFolder(playlistName):
-	if os.path.isdir(playlistName) is False:
-		os.mkdir(playlistName)
-		os.mkdir(os.path.join(playlistName, dirVideoName))
-		print "Folder %s and %s created" % (playlistName, os.path.join(playlistName, dirVideoName))
+def createFolder(playlist_name):
+	if os.path.isdir(playlist_name) is False:
+		os.mkdir(playlist_name)
+		os.mkdir(os.path.join(playlist_name, dirVideoName))
+		print "Folder %s and %s created" % (playlist_name, os.path.join(playlist_name, dirVideoName))
 
 
-def updatePlaylist(config, playlistName):
-	# Get playlist
-	print "Update playlist " + playlistName
-	playlistCode = getPlaylistCode(config, playlistName)
-	if playlistCode is None:
+def updatePlaylist(config, playlist_name):
+	# Get playlist code
+	print "Update playlist " + playlist_name
+	playlist_code = getPlaylistCode(config, playlist_name)
+	if playlist_code is None:
+		print playlist_name + " has no code. Check your playlist.ini"
 		return
 
 	# Create folder if needed
-	createFolder(playlistName)
+	createFolder(playlist_name)
 
 	# Call to youtube-dl
-	os.chdir(os.path.join(playlistName, dirVideoName))
-	os.system('youtube-dl --title --continue --ignore-errors --no-overwrite --no-post-overwrites --keep-video --extract-audio --audio-format=mp3 --audio-quality 0 ' + playlistCode)
-	musicFiles = glob.glob('*.mp3')
+	audio_format = config.get("Config", "audio_format") if config.get("Config", "audio_format") not in ["", None] else "mp3"
+	audio_quality = config.get("Config", "audio_quality") if config.get("Config", "audio_quality") not in ["", None] else "0"
+	command = 'youtube-dl --title --continue --ignore-errors --no-overwrite --no-post-overwrites --keep-video --extract-audio --audio-format ' + audio_format + ' --audio-quality ' + audio_quality + ' ' + playlist_code
+	print command
+	os.chdir(os.path.join(playlist_name, dirVideoName))
+	os.system(command)
+
+	# Move audio file into user directory and replace by empty one
+	musicFiles = glob.glob('*.' + audio_format)
 	for musicFile in musicFiles:
 		if not os.path.isfile(os.path.join(os.pardir, musicFile)):
 			shutil.move(musicFile, os.pardir)
 			open(musicFile, 'w')
+
+	# replace all videos files by empty files, to not dowload them again.
 	videoTypes = ('*.flv', '*.mp4')
 	videoFiles = []
 	for videoType in videoTypes:
@@ -82,7 +91,14 @@ def main():
 	if os.path.isfile(iniFile):
 		config.read(iniFile)
 	else:
-		sys.exit("playlist file does not exist")
+		sys.exit("Playlist file does not exist")
+
+	output_path = config.get("Config", "output_path")
+	if output_path not in ["", None]:
+		if os.path.isdir(output_path) is False:
+			sys.exit(output_path + " does not exist")
+		else:
+			os.chdir(output_path)
 
 	if args.all:
 		updateAllPlaylist(config)
@@ -94,7 +110,7 @@ def main():
 		playlists = []
 		for playlist in config.options('Music'):
 			playlists.append(["", playlist])
-		list_result = PyZenity.List(["select", "playlist"], boolstyle="checklist", data=playlists)
+		list_result = PyZenity.List(["", "playlist"], boolstyle="checklist", data=playlists)
 
 		progression_callback = PyZenity.Progress(text="Downloading videos and converting in mp3")
 
